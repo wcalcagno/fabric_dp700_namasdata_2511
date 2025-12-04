a7e4528dd3c42f410df98b82905d575af1d2e490
Walter E. Calcagno <50029544+wcalcagno@users.noreply.github.com>
2025-12-04 12:04:56 -0300

    Committing 30 items from workspace 0937df67-f304-4d29-bfd4-c5a2bc260840

---
+++ nb_marca_agua_destino.Notebook/notebook-content.py
@@ -0,0 +1,40 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ee3a5cef-859c-4f37-965e-cba92833f8b7",
+# META       "default_lakehouse_name": "Bronze_Landing",
+# META       "default_lakehouse_workspace_id": "0937df67-f304-4d29-bfd4-c5a2bc260840",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "ee3a5cef-859c-4f37-965e-cba92833f8b7"
+# META         },
+# META         {
+# META           "id": "7199137e-ea3a-4d45-87ba-9fafcc3f4154"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# ultimo dato en mi destino
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC select max(time) from Silver_Refined.mciencia_incremental_pipeline
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
