1da9e892cda1c5fc3975caec293349dcae8accb2
Walter E. Calcagno <50029544+wcalcagno@users.noreply.github.com>
2025-12-09 13:43:38 -0300

    Committing 2 items from workspace 0937df67-f304-4d29-bfd4-c5a2bc260840

---
+++ UDF_Namasdata.UserDataFunction/function_app.py
@@ -0,0 +1,47 @@
+import datetime
+import fabric.functions as fn
+import logging
+
+udf = fn.UserDataFunctions()
+
+
+## leer datos desde lakehouse
+# Select 'Manage connections' and add a connection to a Lakehouse.
+# Replace the alias "<My Lakehouse alias>" with your connection alias.
+@udf.connection(argName="myLakehouse", alias="BronzeLanding")
+@udf.function()
+def query_data_from_tables(myLakehouse: fn.FabricLakehouseClient) -> list:
+    '''
+    Description: Query employee data from lakehouse tables and return as JSON objects.
+
+    Args:
+    - myLakehouse (fn.FabricLakehouseClient): Fabric lakehouse connection
+
+    Returns: list: Employee records as dictionaries with EmpName and DepID fields
+    '''
+    # Connect to the Lakehouse SQL Endpoint
+    connection = myLakehouse.connectToSql()
+    
+    # Use connection to execute a query
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT MAX(time) FROM Bronze_landing.dbo.minciencia;")
+    
+    rows = [x for x in cursor]
+    columnNames = [x[0] for x in cursor.description]
+    
+    # Turn the rows into a json object
+    values = []
+    for row in rows:
+        item = {}
+        for prop, val in zip(columnNames, row):
+            if isinstance(val, (datetime.date, datetime.datetime)):
+                val = val.isoformat()
+            item[prop] = val
+        values.append(item)
+
+    # Close the connection
+    cursor.close()
+    connection.close()
+
+    return values
+
